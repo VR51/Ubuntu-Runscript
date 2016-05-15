5c45dc24c45a71d9ccce28ec49e8c1b6f4975cc1
VR51 <github@journalxtra.com>
2016-05-15 02:58:28 +0100

    Initial release into the wild.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Ubuntu-Runscript
-Adds a collection of software repositories to an *buntu distro and installs specific software pacakges. See readme for details of the repositories added and software installed.
+Adds a collection of software repositories to *buntu distros and installs specific software pacakges.
+
+Designed for use with a clean install of Kubuntu or Ubuntu. This is the script I use to create my preferred software environment after clean (re)installation of Kubuntu.
+
+Take note that I'm a web developer. This runscript installs among other software Apache2, PHP7, MariaDB, Webmin and PhpMyAdmin. If you do not need the web server components you will need to delete the sections of the script that install them. I will release a Q&A version of this script soon.
+
+# Version
+
+16.04 Xenial.
+
+If using in a different release of Ubuntu  you will need to update 'xenial' in some repos to whatever the Ubuntu release is.
+
+# How to Use
+Download the script to your desktop. Click the file runscript.sh. If it fails to run check the file is set to be 'executable' or open a terminal and type 'sh runscript.sh' (withuot the quotes).
+
+Use once only. The script does not check whether repos are already installed.
+
+# Repositories Enabled
+* ppa:kubuntu-ppa/ppa
+* ppa:kubuntu-ppa/backports
+* deb http://download.virtualbox.org/virtualbox/debian xenial contrib
+* ppa:ondrej/apache2
+* ppa:ondrej/php
+* http://mirrors.coreix.net/mariadb/repo/10.1/ubuntu xenial main
+* http://ppa.launchpad.net/nijel/phpmyadmin/ubuntu xenial main
+* ppa:nijel/phpmyadmin
+* deb http://dl.google.com/linux/chrome/deb/ stable main
+* ppa:otto-kesselgulasch/gimp
+* ppa:dimula73/krita
+* ppa:webupd8team/y-ppa-manager
+* ppa:openshot.developers/ppa
+* ppa:webcamstudio/webcamstudio-dailybuilds
+
+# Software Installed
+* y-ppa-manager (graphical PPA repository manager)
+* vlc
+* gimp (latest beta version)
+* krita
+* google-chrome-stable
+* webcamstudio
+* virtualbox-5.0 (For Virtual OS Installation)
+* apache2 (Web server)
+* php7.0
+* mariadb-server
+* phpmyadmin
+* openshot-qt (Video Editor)
+* Webmin
+* Mod Pagespeed
---
+++ runscript.sh
@@ -0,0 +1,284 @@
+#!/bin/bash
+clear
+###
+#
+#	Run Script 16.04
+#
+#	Lead Author: Lee Hodson
+#	Donate: paypal.me/vr51
+#	Website: https://github.com/VR51/Ubuntu-Runscript
+#	First Written: 21st Jan. 2016
+#	First Release: 21st Jan. 2016
+#	This Release: 4th May. 2016
+#
+#	Copyright 2016 Lee Hodson
+#	License: GNU
+#
+#	Programmer: Lee Hodson <journalxtra.com>, Lee Hodson <vr51.com>
+#
+#	Use of this program is at your own risk
+#
+#	TO RUN:
+#
+#	- Ensure the script is executable.
+#	- Command line: sh repo-run.sh
+#	- File browser: click runscript.sh
+#
+#	Use Runscript to install base repositories and software after fresh Ubuntu installation.
+#
+###
+
+
+###
+#
+#	EXTRAS
+#
+#	https://www.google.com/linuxrepositories/
+#
+#
+###
+
+
+###
+#
+#	Let the user know we are running
+#
+###
+
+printf "RUNSCRIPT INITIALISED\n------------------------\n\n"
+
+###
+#
+#	Set Variables
+#
+###
+
+# Establish Linux epoch time in seconds
+now=$(date +%s)
+
+# Establish both Date and Time
+todaytime=$(date +"%Y-%m-%d-%H:%M:%S")
+
+# Locate Where We Are
+filepath="$(dirname "$(readlink -f "$0")")"
+
+
+###
+#
+#	Register leave_program function
+#
+###
+
+
+leave_program() {
+
+	exittime=$(date +%s)
+	runtime=$(($exittime - $now))
+
+	printf "\nPROGRAM RUN TIME: $runtime seconds\n"
+	printf "\n\nPress any key to exit"
+	read something
+	exit
+
+}
+
+
+###
+#
+#	Confirm we are running in a terminal
+#		If not, try to launch this program in a terminal
+#
+###
+
+tty -s
+
+if test "$?" -ne 0
+then
+
+	# This code section is released in public domain by Han Boetes <han@mijncomputer.nl>
+	# Updated by Dave Davenport <qball@gmpclient.org>
+	# Updated by Lee Hodson <https://journalxtra.com> - Added break on successful hit, added more terminals, humanized the failure message, replaced call to rofi with printf and made $terminal an array for easy reuse.
+	#
+	# This script tries to exec a terminal emulator by trying some known terminal
+	# emulators.
+	#
+	# We welcome patches that add distribution-specific mechanisms to find the
+	# preferred terminal emulator. On Debian, there is the x-terminal-emulator
+	# symlink for example.
+
+	terminal=( x-terminal-emulator xdg-terminal konsole gnome-terminal terminator urxvt rxvt Eterm aterm roxterm xfce4-terminal termite lxterminal xterm )
+	for i in ${terminal[@]}; do
+		if command -v $i > /dev/null 2>&1; then
+			exec $i -e "$0"
+			break
+		else
+			printf "\nUnable to automatically determine the correct terminal program to run e.g Console or Konsole. Please run this program from a terminal AKA the command line.\n"
+			read something
+			leave_program
+		fi
+	done
+
+fi
+
+###
+#
+#	Obtain Authorisation to update the system
+#
+###
+
+# Add event to run.log
+
+printf "\n\nAUTHORISATION\n-------------\n"
+
+printf "\nAuthorise System Reposet to update the system:\n"
+sudo -v
+
+###
+#
+#	Install repositories and GPG keys
+#
+###
+
+
+## Kubuntu Backports
+
+sudo apt-add-repository ppa:kubuntu-ppa/ppa
+sudo add-apt-repository ppa:kubuntu-ppa/backports
+
+## VirtualBox
+
+sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian xenial contrib" >> /etc/apt/sources.list.d/virtualbox.list'
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+
+## Web Server
+
+sudo apt-add-repository ppa:ondrej/apache2
+sudo apt-add-repository ppa:ondrej/php
+
+sudo apt-get install software-properties-common
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+sudo add-apt-repository 'deb [arch=amd64,i386] http://mirrors.coreix.net/mariadb/repo/10.1/ubuntu xenial main'
+
+sudo sh -c 'echo "deb http://ppa.launchpad.net/nijel/phpmyadmin/ubuntu xenial main" >> /etc/apt/sources.list.d/nijel-ubuntu-php-myadmin-xenial.list'
+sudo sh -c 'echo "deb-src http://ppa.launchpad.net/nijel/phpmyadmin/ubuntu xenial main" >> /etc/apt/sources.list.d/nijel-ubuntu-php-myadmin-xenial.list'
+
+
+## PhpMyAdmin
+
+sudo apt-add-repository ppa:nijel/phpmyadmin
+
+## Webmin
+# PPA is still on 'sarge' so we download and install the .deb package (further down this page).
+# Update from within Webmin.
+
+# sudo echo "\n# Webmin" >> /etc/apt/sources.list
+# sudo echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
+# sudo echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" >> /etc/apt/sources.list
+# wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
+
+## Google Chrome
+
+wget -q https://dl-ssl.google.com/linux/linux_signing_key.pub -O- | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+## Gimp
+# Beta versions
+
+sudo apt-add-repository ppa:otto-kesselgulasch/gimp
+
+## Krita
+# https://krita.org/download/krita-desktop/
+
+sudo add-apt-repository ppa:dimula73/krita
+
+## Web Update YPPA Manager
+
+sudo add-apt-repository ppa:webupd8team/y-ppa-manager
+
+## Openshot Video Editor
+
+sudo add-apt-repository ppa:openshot.developers/ppa
+
+## Webcamstudio
+
+sudo add-apt-repository ppa:webcamstudio/webcamstudio-dailybuilds
+
+###
+#
+#	Update Everything
+#
+###
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+
+###
+#
+#	Install Packages.
+#       Each to a line to make it easy to follow
+###
+
+sudo apt-get install y-ppa-manager
+sudo apt-get install vlc
+sudo apt-get install gimp
+sudo apt-get install krita
+sudo apt-get install google-chrome-stable
+sudo apt-get install webcamstudio
+sudo apt-get install virtualbox-5.0
+sudo apt-get install apache2
+sudo apt-get install php7.0
+sudo apt-get install mariadb-server
+sudo apt-get install php7.0-mysql
+sudo apt-get install phpmyadmin
+sudo apt-get install openshot-qt
+
+## Webmin dependencies
+sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+
+## Install Webmin
+# PPA is still on 'sarge' so we download and install the .deb package. Update from within Webmin.
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.791_all.deb
+sudo dpkg --install webmin_1.791_all.deb
+
+
+## Mod_Pagespeed
+
+wget https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-stable_current_amd64.deb
+sudo dpkg -i mod-pagespeed-*.deb
+sudo apt-get -f install
+
+## Google Chrome
+
+
+###
+#
+#	Advise Status Complete
+#
+###
+
+printf "\nSOFTWARE INSTALLED\n"
+
+prompt_confirm() {
+  while true; do
+    read -r -n 1 -p "${1:-Continue?} [y/n]: " REPLY
+    case $REPLY in
+      [yY])
+		sudo apt-get clean
+		sudo apt-get autoremove
+		printf "Press any key to exit."
+		read something
+		leave_program
+		;;
+
+      [nN])
+		leave_program
+		;;
+
+      *)
+		printf "\033[31m %s \n\033[0m" "Answer 'y' to proceed or 'n' to exit."
+    esac
+  done
+}
+
+prompt_confirm "Clean the cache of downloaded packages and run autoremove?" || exit 0
